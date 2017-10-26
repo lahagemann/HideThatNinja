e82c4e12d5a130d532135d284b39c023866af633
Carolina Mello <carollangemello@gmail.com>
2017-10-26 20:53:00 -0200

    Transformação adicionada e ninja não anda quando ta transformado

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,20 +6,41 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Sprite[] spriteArray;
+	private SpriteRenderer spriteRenderer;
+	private bool canWalk;
    
     private Rigidbody2D rb2d;
 
 	void Start()
 	{
+		canWalk = true;
 		rb2d = GetComponent<Rigidbody2D> ();
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		spriteRenderer.sprite = spriteArray[0];
     }
 
 	void FixedUpdate()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-		rb2d.AddForce (movement * speed);
+		if (canWalk)
+		{
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
+			Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+			rb2d.AddForce (movement * speed);
+			TransformNinja ();
+		}
 	}
 
+	void TransformNinja()
+	{
+		if (Input.anyKey)
+		{
+			if (Input.GetButton ("Submit"))
+			{
+				spriteRenderer.sprite = spriteArray[1];
+				canWalk = false;
+			}
+		}
+	}
 }
