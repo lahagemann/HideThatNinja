12c63f3422b3ae060b00b9018758ef3429c2cda1
Carolina Mello <carollangemello@gmail.com>
2017-11-08 01:40:10 -0200

    Tentando fazer o inimigo andar, mas não ta rolando

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,15 +10,38 @@ public class EnemyController : MonoBehaviour {
 	public float viewDistance;
 	public float viewAngle;
 	private GameObject player;
+
+	public GameObject[] waypoints;
+	private int currentWaypoint = 0;
+	private float lastWaypointSwitchTime;
+	public float speed = 1.0f;
 	
 	void Start(){
 		gameOver.text = "";
 		player = GameObject.Find("PlayerNinja");
+		lastWaypointSwitchTime = Time.time;
 		
 	}
 	
 	void FixedUpdate(){
 		checkSight();
+
+		Vector3 startPosition = waypoints [currentWaypoint].transform.position;
+		Vector3 endPosition = waypoints [currentWaypoint + 1].transform.position;
+
+		float pathLength = Vector3.Distance (startPosition, endPosition);
+		float totalTimeForPath = pathLength / speed;
+		float currentTimeOnPath = Time.time - lastWaypointSwitchTime;
+		gameObject.transform.position = Vector3.Lerp (startPosition, endPosition, currentTimeOnPath / totalTimeForPath);
+ 
+		if (gameObject.transform.position.Equals(endPosition)) {
+			if (currentWaypoint < waypoints.Length - 2) {
+				currentWaypoint++;
+				lastWaypointSwitchTime = Time.time;
+			} else { 
+				//Destroy(gameObject);
+			}
+		}
 	}
 	
 	void checkSight(){
@@ -31,5 +54,4 @@ public class EnemyController : MonoBehaviour {
 		}
 		
 	}
-	
 }
